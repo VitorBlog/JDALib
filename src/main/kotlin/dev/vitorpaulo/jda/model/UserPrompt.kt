e6f6c93ae5ce1887c334cc1d889971938e73548b
vitor <vitorblog12@gmail.com>
2021-07-14 12:59:47 -0300

    :truck: Moving functions to builder

--- src/main/kotlin/dev/vitorpaulo/jda/model/UserPrompt.kt
+++ src/main/kotlin/dev/vitorpaulo/jda/model/UserPrompt.kt
@@ -9,27 +9,34 @@ import java.util.function.Consumer
 class UserPrompt(val user: User, val textChannel: TextChannel) {
 
     val id = "${user.id}:${textChannel.id}"
-    var validator: () -> Boolean = { true }
+    var validator: (String) -> Boolean = { true }
     var cancelSequences = arrayListOf<String>()
     var invalidCallback: Consumer<Message>? = null
     var canceledCallback: Consumer<Message>? = null
     var successCallback: Consumer<Message>? = null
 
-    fun validator(validator: () -> Boolean) = apply { this.validator = validator; return this }
+    class Builder(user: User, textChannel: TextChannel) {
 
-    fun addCancelSequence(string: String) = apply { this.cancelSequences.add(string); return this }
+        private val prompt = UserPrompt(user, textChannel)
 
-    fun cancelSequences(cancelSequences: ArrayList<String>) = apply { this.cancelSequences = cancelSequences; return this }
+        fun validator(validator: (String) -> Boolean) = apply { prompt.validator = validator; return this }
 
-    fun invalidCallback(invalidCallback: Consumer<Message>) = apply { this.invalidCallback = invalidCallback; return this }
+        fun addCancelSequence(string: String) = apply { prompt.cancelSequences.add(string); return this }
 
-    fun canceledCallback(canceledCallback: Consumer<Message>) = apply { this.canceledCallback = canceledCallback; return this }
+        fun cancelSequences(cancelSequences: ArrayList<String>) = apply { prompt.cancelSequences = cancelSequences; return this }
 
-    fun successCallback(successCallback: Consumer<Message>) = apply { this.successCallback = successCallback; return this }
+        fun invalidCallback(invalidCallback: Consumer<Message>) = apply { prompt.invalidCallback = invalidCallback; return this }
 
-    fun queue() {
+        fun canceledCallback(canceledCallback: Consumer<Message>) = apply { prompt.canceledCallback = canceledCallback; return this }
 
-        PromptDao.add(this)
+        fun successCallback(successCallback: Consumer<Message>) = apply { prompt.successCallback = successCallback; return this }
+
+        fun queue(): UserPrompt {
+
+            PromptDao.add(prompt)
+            return prompt
+
+        }
 
     }
 
